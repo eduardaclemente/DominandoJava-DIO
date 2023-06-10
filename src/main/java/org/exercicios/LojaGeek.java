315ff16be0c9d1a9cb257fd2ac0f1ec09a46b775
Eduarda Clemente <eduardaclemente.369@gmail.com>
2023-06-10 17:44:46 -0300

    First commit

---
+++ src/main/java/org/exercicios/LojaGeek.java
@@ -0,0 +1,25 @@
+package org.exercicios;
+
+// - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next";
+// - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.
+
+import java.util.Scanner;
+public class LojaGeek {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double media = scanner.nextDouble();
+
+        //TODO: Verifique a média lida e imprima a avaliação do funcionário.
+        //Onde: "APR" é bom, "MED" é médio e "REP" é ruim.
+        if (media < 5) {
+            System.out.println("REP");
+        } else if (media < 7) {
+            System.out.println("MED");
+        } else {
+            System.out.println("APR");
+        }
+    }
+
+}
